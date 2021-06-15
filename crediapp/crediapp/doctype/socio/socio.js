5f899831d185138edaba008aa8dc91db1776f5f6
Orlando Cholota <edwin_orlando83@hotmail.com>
2021-06-15 06:47:32 -0500

    cambios

--- crediapp/crediapp/doctype/socio/socio.js
+++ crediapp/crediapp/doctype/socio/socio.js
@@ -5,6 +5,15 @@ frappe.ui.form.on('Socio', {
 	refresh: function (frm) {
 		calcular(frm);
 	},
+	onload:function(frm){
+
+		get_provincia(frm);
+		if (frm.doc.provincia) get_cantones(frm.doc.provincia, frm, "canton");
+		if (frm.doc.canton ) get_parroquias (frm.doc.canton , frm);
+	},
+	provincia:function(frm){
+		get_cantones(frm.doc.provincia, frm, "canton");
+	},
 	onload_post_render: function (frm) {
 		controlSoloNumeroEntero(frm.fields_dict.cedula);
 		controlSoloNumeroEntero(frm.fields_dict.cedulaconyuge);
@@ -17,8 +26,8 @@ frappe.ui.form.on('Socio', {
 			frappe.throw("Cedula incorrecto del socio");
 		}
 
-	 
-		if ( frm.doc.cedulaconyuge && validarCedula(frm.doc.cedulaconyuge) == 0) {
+
+		if (frm.doc.cedulaconyuge && validarCedula(frm.doc.cedulaconyuge) == 0) {
 
 			frappe.throw("Cedula incorrecto del conyuge");
 		}
@@ -42,16 +51,58 @@ function calcular(frm) {
 		frm.fields_dict.fecha_nac.set_label('Fecha de nacimiento ');
 	}
 }
-	function getEdad(dateString) {
-		let hoy = new Date();
-		let fechaNacimiento = new Date(dateString);
-		let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
-		let diferenciaMeses = hoy.getMonth() - fechaNacimiento.getMonth();
-		if (
-			diferenciaMeses < 0 ||
-			(diferenciaMeses === 0 && hoy.getDate() < fechaNacimiento.getDate())
-		) {
-			edad--;
-		}
-		return edad;
+function getEdad(dateString) {
+	let hoy = new Date();
+	let fechaNacimiento = new Date(dateString);
+	let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+	let diferenciaMeses = hoy.getMonth() - fechaNacimiento.getMonth();
+	if (
+		diferenciaMeses < 0 ||
+		(diferenciaMeses === 0 && hoy.getDate() < fechaNacimiento.getDate())
+	) {
+		edad--;
 	}
+	return edad;
+}
+
+
+
+function get_provincia(  frm) {
+	frappe.call({
+		 
+		method: "crediapp.crediapp.doctype.dpa.dpa.get_provincias",
+		callback: (r) => {
+		  frm.set_df_property("provincia", "options", r.message);
+		  frm.set_df_property("emp_provincia", "options", r.message);
+		  frm.set_df_property("cony_provincia", "options", r.message);
+		  frm.set_df_property("prov_emp_cony", "options", r.message);
+		  frm.refresh_field("provincia");
+		  frm.refresh_field("emp_provincia");
+		  frm.refresh_field("cony_provincia");
+		  frm.refresh_field("prov_emp_cony");
+
+		},
+	  });
+  }
+
+function get_cantones(incod_prov, frm, control) {
+	frappe.call({
+	  method: "crediapp.crediapp.doctype.dpa.dpa.get_cantones",
+	  args: { cod_prov: incod_prov },
+	  callback: (r) => {
+		frm.set_df_property(control, "options", r.message);
+		frm.refresh_field(control);
+	  },
+	});
+  }
+
+  function get_parroquias(incod_prov, frm) {
+	frappe.call({
+	  method: "crediapp.crediapp.doctype.dpa.dpa.get_parroquia",
+	  args: { cod_can: incod_prov },
+	  callback: (r) => {
+		frm.set_df_property("parroquia", "options", r.message);
+		frm.refresh_field("parroquia");
+	  },
+	});
+  }
